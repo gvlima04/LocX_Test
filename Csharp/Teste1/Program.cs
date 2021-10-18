ccec77c37398a47f1ac6ab7f9d5cd4cd79baa2a4
Guilherme V. Lima <guilhermevlima04@outlook.com>
2021-10-18 19:28:06 -0300

    Initial commit

---
+++ Csharp/Teste1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Teste1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec;
+
+            Console.WriteLine("Digite o tempo em segundos: ");
+            bool cvOk = Int32.TryParse((Console.ReadLine()), out sec);
+
+            if (cvOk)
+            {
+                Sec2hour(sec);
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível converter o valor informado para inteiro.");
+            }
+
+        }
+
+        static void Sec2hour(int sec)
+        {
+            int h = (int)sec / 3600; //O tempo em horas (h) será dado pelo valor inteiro da divisão do valor de entrada por 3600 (1 hora = 3600 segundos)
+
+            int hResto = sec % 3600; //O resto da divisão corresponde aos minutos e segundo restantes
+
+            int m = (int)hResto / 60; // O valor da inteiro de h_resto/60 será o tempo em minuto 
+            int s = hResto % 60; //O resto da divisão será o tempo em segundos
+
+            Console.WriteLine("{0}:{1}:{2}", h, m, s);
+        }
+    }
+}
---
+++ Csharp/Teste2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Teste2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com os parâmetros no formato <QtdProp> <ValorAluguel> <TaxaAdm>: "); //Teste: 2 2000 10; 1 3500 8
+            string parm = Console.ReadLine();
+
+            Console.WriteLine(ValorProprietario(parm));
+        }
+
+        static string ValorProprietario(string parm)
+        {
+            string[] subs = parm.Split(' ');
+
+            int qtdProp = Int32.Parse(subs[0]);
+            float aluguelTotal = float.Parse(subs[1]);
+            float txAdmin = float.Parse(subs[2]);
+
+            float aliq = 0;
+            float ded = 0;
+
+            //Cálculo da Taxa de Administração
+            float dTxAdmin = aluguelTotal * (txAdmin / 100);
+
+            //Verificando a tabela de alíquota e dedução de imposto de renda (por proprietário)  
+            float valPorProp = aluguelTotal / qtdProp;
+            switch (valPorProp)
+            {
+                case float n when (n <= 1903.98):
+                    aliq = 0;
+                    ded = 0;
+                    break;
+                case float n when (n >= 1903.99 & n <= 2826.65):
+                    aliq = 0.075f;
+                    ded = 142.80f;
+                    break;
+                case float n when (n >= 2826.66 & n <= 3751.05):
+                    aliq = 0.15f;
+                    ded = 354.80f;
+                    break;
+                case float n when (n >= 3751.06 & n <= 4664.68):
+                    aliq = 0.225f;
+                    ded = 636.13f;
+                    break;
+                case float n when (n >= 4664.69):
+                    aliq = 0.275f;
+                    ded = 869.36f;
+                    break;
+            }
+
+            //Calculo do desconto do imposto de renda
+            float dIR = aluguelTotal * (aliq);
+
+            //Valor recebido por proprietário
+            float valProp = (aluguelTotal - dTxAdmin - dIR + ded) / qtdProp;
+
+            //Escrevendo o valor de saída no formato solicitado
+            string ret = "";
+            for (int i = 1; i <= qtdProp; i++)
+            {
+                ret = ret + string.Format("{0:N2}", valProp) + " ";
+            }
+
+            return ret;
+        }
+    }
+}
